140d1ee4c7c5c43d8308b2be0454116054806194
Paul Robinson <paul.robinson@redhat.com>
2021-09-07 13:47:18 +0100

    Removed .git directory from consideration as not in source code

--- report.sh
+++ report.sh
@@ -2,6 +2,7 @@
 
 GH_ORG=$1
 REPORT="$PWD/$2"
+GREP_BASE_CMD="grep --exclude-dir='.git' -rni"
 
 if [ "$2" == "" ]; then
   echo "Usgae: $0 <GH Org name> <Report output path>"
@@ -11,7 +12,7 @@ fi
 rm -rf /tmp/consc
 mkdir /tmp/consc
 
-echo "Repo, Master, Slave, Blacklist, Whitelist" > $REPORT
+echo "Repo, Branch, Master, Slave, Blacklist, Whitelist" > $REPORT
 
 REPOS=$(gh repo list $GH_ORG | awk -F ' ' ' { print $1 }')
 
@@ -22,12 +23,15 @@ for REPO in $REPOS; do
   cd /tmp/consc
   git clone "git@github.com:$REPO"
   cd $REPO_NAME
-  
-  MASTER_COUNT=$(grep -rni 'master' . | wc -l)
-  SLAVE_COUNT=$(grep -rni 'slave' . | wc -l)
-  BLACKLIST_COUNT=$(grep -rni 'blacklist' . | wc -l)
-  WHITELIST_COUNT=$(grep -rni 'whitelist' . | wc -l)
+ 
+  DEFAULT_BRANCH=$(git branch --show-current);
+
+  rm -rf .git 
+  MASTER_COUNT=$($GREP_BASE_CMD 'master' . | wc -l)
+  SLAVE_COUNT=$($GREP_BASE_CMD 'slave' . | wc -l)
+  BLACKLIST_COUNT=$($GREP_BASE_CMD 'blacklist' . | wc -l)
+  WHITELIST_COUNT=$($GREP_BASE_CMD 'whitelist' . | wc -l)
 
 
-  echo "$REPO_NAME,$MASTER_COUNT,$SLAVE_COUNT,$BLACKLIST_COUNT,$WHITELIST_COUNT" >> $REPORT
+  echo "$REPO_NAME,$DEFAULT_BRANCH,$MASTER_COUNT,$SLAVE_COUNT,$BLACKLIST_COUNT,$WHITELIST_COUNT" >> $REPORT
 done
